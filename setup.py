e746360d2be782593e9b0a9f1993604d95c345e4
albertkjoller <albert_k_jacobsen@hotmail.com>
2023-08-28 10:30:53 +0200

    Initialize repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Methods related to Active Bayesian Deep Learning, including use cases on various data sets.',
+    author='albertkjoller',
+    license='MIT',
+)
